a47d5eb30825cff3d4b82bdbcf9b9e91c65d8316
Rene Montes de Oca <reneamontes@gmail.com>
2018-02-24 22:46:04 -0500

    Testing createShoe endpoint

--- app.go
+++ app.go
@@ -93,7 +93,7 @@ func (a *App) sayHello(w http.ResponseWriter, r *http.Request) {
 
 func (a *App) createShoe(w http.ResponseWriter, r *http.Request) {
     
-    log.Println(a.Model.truetosizemodoel)
+    log.Println(a.Model)
 
     /*shoe := model.Shoe
 
